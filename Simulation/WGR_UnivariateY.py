568868a284af5a78efbc3bf2a9622b0a747d8261
Tong273 <t.wang@link.cuhk.edu.hk>
2025-04-11 11:24:16 -0400

    Update WGR_UnivariateY.py

--- Simulation/WGR_UnivariateY.py
+++ Simulation/WGR_UnivariateY.py
@@ -8,9 +8,11 @@ sys.path.append(parent_dir)
 incase the above code does not work, you can use the absolute path instead
 sys.path.append(r".\")
 """
+
 import torch
 import torch.nn as nn
 import numpy as np
+import pandas as pd
 from torch.utils.data import TensorDataset, DataLoader
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau 
@@ -24,11 +26,11 @@ from models.discriminator import discriminator_fnn
 
 import argparse
 
-```
+
 if 'ipykernel_launcher.py' in sys.argv[0]:  #if not work in jupyter, you can delete this part
     import sys
     sys.argv = [sys.argv[0]] 
-```
+
 
 parser = argparse.ArgumentParser(description='Implementation of WGR for M1')
 
@@ -46,8 +48,8 @@ parser.add_argument('--test', default=1000, type=int, help='size of test dataset
 parser.add_argument('--train_batch', default=128, type=int, metavar='BS', help='batch size while training')
 parser.add_argument('--val_batch', default=100, type=int, metavar='BS', help='batch size while validation')
 parser.add_argument('--test_batch', default=100, type=int, metavar='BS', help='batch size while testing')
-parser.add_argument('--epochs', default=100, type=int, help='number of epochs to train')
-parser.add_argument('--reps', default=100, type=int, help='number of replications')
+parser.add_argument('--epochs', default=200, type=int, help='number of epochs to train')
+parser.add_argument('--reps', default=2, type=int, help='number of replications')
 
 args = parser.parse_args()
 
@@ -59,16 +61,15 @@ print(args)
 
 setup_seed(1234)
 reps=(args.reps,)
-seed = torch.randint(0, 10000, args.reps)  
+seed = torch.randint(0, 10000, reps)  
 
 def main():
     test_G_mean_sd = []  # Initialize as list
     test_G_quantile = []  # Initialize as list
 
-    for k in range(args.reps):
+    for k in range(2):
         print('============================ REPLICATION ==============================')
         print(k, seed[0])
-        print("m: " + str(sorted_list[k]))
 
         setup_seed(seed[0].detach().numpy().item())
         
@@ -90,6 +91,8 @@ def main():
         test_dataset = TensorDataset( test_X.float(), test_Y.float() )
         loader_test  = DataLoader(test_dataset , batch_size=args.test_batch, shuffle=True)
 
+        global G_net, D_net, trained_G, trained_D
+
         # Define generator network and discriminator network
         G_net = generator_fnn(Xdim=args.Xdim, Ydim=args.Ydim, noise_dim=args.noise_dim, hidden_dims = [64, 32])
         D_net = discriminator_fnn(input_dim=args.Xdim+args.Ydim, hidden_dims = [64, 32])
@@ -101,18 +104,20 @@ def main():
         # Training
         trained_G, trained_D = train_WGR_fnn(D=D_net, G=G_net, D_solver=D_solver, G_solver=G_solver, loader_train = loader_train, 
                                              loader_val=loader_val, noise_dim=args.noise_dim, Xdim=args.Xdim, Ydim=args.Ydim, 
-                                             batch_size=args.train_batch, save_path='./', model_type=args.model, device='cpu', num_epochs=50)
-
+                                             batch_size=args.train_batch, save_path='./', model_type=args.model, device='cpu', num_epochs=args.epochs)
+        
         # Calculate the L1 and L2 error, MSE of conditional mean and conditional standard deviation on the test data  
         mean_sd_result = L1L2_MSE_mean_sd_G(G = trained_G,  test_size = args.test, noise_dim=args.noise_dim, Xdim=args.Xdim, 
                                             batch_size=args.test_batch,  model_type=args.model, loader_dataset = loader_test )
+        
 
         # Calculate the MSE of conditional quantiles at different levels.
         quantile_result = MSE_quantile_G_uniY(G = trained_G, loader_dataset = loader_test , noise_dim=args.noise_dim, Xdim=args.Xdim,
                                               test_size = args.test,  batch_size=args.test_batch, model_type=args.model)
 
-        test_G_mean_sd.append(mean_sd_result.detach().cpu().numpy())
-        test_G_quantile.append(quantile_result.detach().cpu().numpy())
+        #test_G_mean_sd.append(mean_sd_result.detach().cpu().numpy())
+        test_G_mean_sd.append(np.array(mean_sd_result))
+        test_G_quantile.append(np.array(quantile_result))
 
     print("L1 error, L2 error, MSE(mean), MSE(sd):", test_G_mean_sd)
     print("MSE(quantile) at level {0.05, 0.25, 0.50, 0.75, 0.95}:", test_G_quantile)
@@ -122,7 +127,8 @@ def main():
     test_G_quantile_csv = pd.DataFrame(test_G_quantile)
 
     test_G_mean_sd_csv.to_csv("./test_G_mean_sd_"+str(args.model)+"_d"+str(args.Xdim)+".csv")
-    test_G_quantile_csv.to_csv("./M2d100/test_G_"+str(args.model)+"_d"+str(args.Xdim)+".csv")
+    test_G_quantile_csv.to_csv("./test_G_quantile_"+str(args.model)+"_d"+str(args.Xdim)+".csv")
+
 
 
 #run
